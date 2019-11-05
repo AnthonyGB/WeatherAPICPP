6f2981d338a19d76c0437ef529e8a0a95f47b1e8
AnthonyBroussard <broussardanthony11022@gmail.com>
2019-11-05 10:45:06 -0600

    Changed from xml parser to json parser. Added struct for Weather Data

--- WeatherApp/Weather.h
+++ WeatherApp/Weather.h
@@ -19,7 +19,7 @@ public:
 
 private:
 	//there's more secure ways to do this but since this is for school, I don't want to bother so iput the api key in here
-	string url = "api.openweathermap.org/data/2.5/weather?q={" + cityName + "},{" + countryCode + "}&APPID={f2cc4ae311506f4fae2685b91b863ef6}&units=imperial"; //for celsius units=metric
+	string url = "api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + countryCode + "&APPID=f2cc4ae311506f4fae2685b91b863ef6&units=imperial"; //for celsius units=metric
 	/* if we want to pass an id instead for better accuracy this will be the url...
 	"http://api.openweathermap.org/data/2.5/forecast?id={ city id}&APPID={APIKEY}"
 	*/
--- WeatherApp/WeatherApp.cpp
+++ WeatherApp/WeatherApp.cpp
@@ -1,11 +1,15 @@
 // WeatherApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-//https://curl.haxx.se/libcurl/c/example.html some examples of libcurl being used. I suggest looking through it along with documentation
-//tinyxml will allow us to parse the info we get from curl and output it to console. If you prefer a different XML parser like pugi let me know and ill switch it.
-//I have not looked extensively at the documentation so i'll have to learn how to use them but they work
+// Install cpr (c requests for people library)
+// and rapidJSON NOT pugixml. TYSM (:
+
+
+
+
 #define CURL_STATICLIB
 #include <iostream>
-#include <pugixml.hpp>
+//#include <pugixml.hpp>
+#include <rapidjson/document.h>
 #include "Weather.h";
 #include "UI.h";
 #include <cpr/cpr.h>
@@ -13,10 +17,11 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
-	auto request = cpr::Get(cpr::Url{"api.openweathermap.org/data/2.5/weather?q={London},{uk}&APPID={f2cc4ae311506f4fae2685b91b863ef6}&units=imperial"});
-
-	displayIntroduction();
-	displayMenu();
+	auto request = cpr::Get(cpr::Url{"api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f2cc4ae311506f4fae2685b91b863ef6&units=imperial"});
+	string rString = request.text; //JSON text string
+	cout << rString;
+	//displayIntroduction();
+	//displayMenu();
 	//pugi::xml_attribute_struct;
 	return 0;
 }
